12967f3515544c396ad2e9d209b80f5c55c4f998
Jacqueline45 <65167552+Jacqueline45@users.noreply.github.com>
2020-05-24 13:00:47 +0800

    Update token.sol

--- contracts/token.sol
+++ contracts/token.sol
@@ -15,8 +15,8 @@ contract SproutOwnership is Sprout, ERC20 {
   function _transfer(address sender, address recipient, uint256 amount) internal {
     require(sender != address(0), "ERC20: transfer from the zero address");
     require(recipient != address(0), "ERC20: transfer to the zero address");
-    balance[sender] = balances[sender].sub(amount, "ERC20: transfer amount exceeds balance");
-    balance[recipient] = balances[recipient].add(amount);
+    balance[sender] = balance[sender].sub(amount);
+    balance[recipient] = balance[recipient].add(amount);
     _totalSupply = _totalSupply.add(amount);
     emit Transfer(sender, recipient, amount);
   }
@@ -43,7 +43,7 @@ contract SproutOwnership is Sprout, ERC20 {
   
   function transferFrom(address sender, address recipient, uint256 amount) public returns (bool) {
         _transfer(sender, recipient, amount);
-        _approve(sender, msg.sender, _allowances[sender][msg.sender].sub(amount, "ERC20: transfer amount exceeds allowance"));
+        _approve(sender, msg.sender, _allowances[sender][msg.sender].sub(amount));
         return true;
    }
    
