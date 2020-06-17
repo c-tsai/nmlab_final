4e9e1715603aa8d9971ef09fcfd1d55a2a313ce1
Jacqueline45 <65167552+Jacqueline45@users.noreply.github.com>
2020-06-17 22:40:14 +0800

    Update 2_sprout_migration.js

--- migrations/2_sprout_migration.js
+++ migrations/2_sprout_migration.js
@@ -1,23 +1,4 @@
-const SafeMath = artifacts.require("SafeMath");
-const Ownable = artifacts.require("Ownable");
-const Sprout = artifacts.require("Sprout");
-const OwnerItem = artifacts.require("OwnerItem");
-const SproutOwnership = artifacts.require("SproutOwnership");
 const SproutMarket = artifacts.require("SproutMarket");
-const ERC20 = artifacts.require("ERC20");
-
 module.exports = function(deployer) {
-  deployer.deploy(SafeMath);
-  deployer.deploy(Ownable);
-  deployer.deploy(ERC20);
-  deployer.link(SafeMath, [Sprout, SproutOwnership]);
-  deployer.link(Ownable, Sprout);
-  deployer.deploy(Sprout);
-  deployer.link(ERC20, SproutOwnership);
-  deployer.link(Sprout, SproutOwnership);
-  deployer.deploy(SproutOwnership);
-  deployer.link(SproutOwnership, OwnerItem);
-  deployer.deploy(OwnerItem);
-  deployer.link(OwnerItem, SproutMarket);
   deployer.deploy(SproutMarket);
 };
